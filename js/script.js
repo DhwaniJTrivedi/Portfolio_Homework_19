15c4160217fdf69c546adff3a92cf5aa7226f471
DhwaniJTrivedi <127465429+DhwaniJTrivedi@users.noreply.github.com>
2023-07-27 16:29:41 -0400

    added animated button

--- js/script.js
+++ js/script.js
@@ -13,23 +13,6 @@ function scrollToContent(tabNumber) {
     });
   }
   
-//   window.addEventListener("DOMContentLoaded", loading)
-// function loading() {
-//   var percents = [0.25,0.5,0.75,1],
-//     step = 0,
-//     truckLoopDur = 10,
-//       fill = function() {
-//       let fillEl = document.querySelector(".progress-fill");
-//       fillEl.style.transform = "scaleY(" + percents[step] + ")";
-//       ++step;
-
-//       if (step < percents.length) {
-//         setTimeout(fill, (truckLoopDur * 1e3)/2);
-//       }
-//       };
-//   setTimeout(fill, (truckLoopDur * 1e3)/4);
-// }
-
 window.addEventListener("DOMContentLoaded", loading);
 
   function loading() {
@@ -55,3 +38,15 @@ window.addEventListener("DOMContentLoaded", loading);
     setTimeout(fill, (truckLoopDur * 1e3) / 4);
   }
    
+  document.querySelector(".container_subtrack").addEventListener("click", () => {
+    // Open the link in a new window
+    window.open("https://docs.google.com/presentation/d/194sUtutDolPjAD480f1m5HSbz1_ydFZWQ4CVtW-dW1s/edit?usp=sharing", "_blank");
+  });
+  document.querySelector(".container_artscape").addEventListener("click", () => {
+    // Open the link in a new window
+    window.open("https://docs.google.com/presentation/d/1m3prUtj1v4DuwcZ7XicW9-4ni3KNs65TypE2CmCusFQ/edit?usp=sharing", "_blank");
+  });
+  document.querySelector(".container_usdoi").addEventListener("click", () => {
+    // Open the link in a new window
+    window.open("https://docs.google.com/presentation/d/1pl4L6lyyOs5nXY_gO50KO2ziFp5Tg-F8cURR_SLkcPg/edit?usp=sharing", "_blank");
+  });
